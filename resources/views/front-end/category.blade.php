d01202d9f78383bf9bca11ab0f51a9d8af672e9f
ruaa <ruaa.reda15@gmail.com>
2023-04-17 03:23:07 +0200

    design

--- resources/views/front-end/category.blade.php
+++ resources/views/front-end/category.blade.php
@@ -8,24 +8,27 @@
         <title>Document</title>
         <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+            integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="{{asset('web/css/category.css')}}">
-        <link rel="stylesheet" href="{{asset('web/css/style.css')}}"  >
-    </head>
-    <style>
-        .b {
-            display:table;
-            width:100%;
+        <link rel="stylesheet" href="{{asset('web/css/style.css')}}">
+        <style>
+        body {
+            background-image: url("{{url('/')}}/web/imgs/31-layers.png");
+            background-size: 115%;
+            background-repeat: repeat-y;
+            background-position-x: -30px;
         }
-        .b span {
-            display:table-cell;
-            vertical-align:middle;
-            padding: 10px 5px 10px 10px;
-        }
-        .b h4 {
-            padding: 10px 10px 10px 5px;
-            word-break:break-all;
+
+        @media (min-width: 576px) {
+            body {
+                background-size: 110%;
+            }
         }
         </style>
+    </head>
+
     <body>
         <div class="categories">
 
@@ -35,30 +38,31 @@
             <div class="cate-item">
                 <div class="container">
 
-                    <div class="b" style="border: 1px solid rgba(51, 51, 51, 0.4); background: rgba(51, 51, 51, 0.1);">
+                    <div class="b"
+                        style="border: 1px solid rgba(51, 51, 51, 0.4); background: rgba(51, 51, 51, 0.1); margin-bottom: 40px;">
 
-                        <h4 class="heading" style="text-align: center;">
+                        <h4 class="heading" style="text-align: center; text-transform: capitalize;">
                             {{$category->section->name_en}}
                         </h4>
                     </div>
                     <div class="cate-wrapper d-flex gx-4 align-items-center">
                         <div class="img-wrapper">
-                            <img src="{{ url('public/Image/'.$category->image) }}"
-                            style="height: 150px; width: 150px;">
+                            <img src="{{ url('public/Image/'.$category->image) }}">
                         </div>
-                        <div class="description" style="margin-top:5px">
+                        <div class="description">
                             <h4>{{$category->name_en}}</h4>
-                            <h4>{{$category->descrptions_en}}</h4>
-                            <p>{{$category->price}} <img style="margin-top:5px" width="7%" src="{{asset('web/imgs/1.png')}}" alt=""></p>
-
-                            {{-- <i class="fa fa-dollar" style="font-size:48px;color:red"></i> --}}
+                            <h5>{{$category->descrptions_en}}</h5>
+                            <div class="div price-wrapper d-flex align-items-center">
+                                <p class="mb-0 me-1">{{$category->price}}</p>
+                                <i class="fa-solid fa-dollar-sign" style="color: #C2B280;"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
 
-@endforeach
+            @endforeach
 
             @endif
 
@@ -69,62 +73,38 @@
             <div class="cate-item">
                 <div class="container">
 
-                    <div class="b" style="border: 1px solid rgba(51, 51, 51, 0.4); background: rgba(51, 51, 51, 0.1);">
+                    <div class="b"
+                        style="border: 1px solid rgba(51, 51, 51, 0.4); background: rgba(51, 51, 51, 0.1); margin-bottom: 40px;">
 
-                        <h4 class="heading" style="text-align: center;">
+                        <h4 class=" heading" style="text-align: center;">
                             {{$category->section_ar->name_ar}}
                         </h4>
                     </div>
                     <div class="cate-wrapper d-flex gx-4 align-items-center">
                         <div class="img-wrapper">
-                            <img src="{{ url('public/Image/'.$category->image) }}"
-                            style="height: 150px; width: 150px;">
+                            <img src="{{ url('public/Image/'.$category->image) }}">
                         </div>
-                        <div class="description" style="margin-top:5px">
+                        <div class="description">
                             <h4>{{$category->name_ar}}</h4>
-                            <h4>{{$category->descrptions_ar}}</h4>
-                            <p>{{$category->price}} <img style="margin-top:5px" width="7%" src="{{asset('web/imgs/1.png')}}" alt=""></p>
-
-                            {{-- <i class="fa fa-dollar" style="font-size:48px;color:red"></i> --}}
+                            <h5>{{$category->descrptions_ar}}</h5>
+                            <div class="div price-wrapper d-flex align-items-center">
+                                <p class="mb-0 me-1">{{$category->price}}</p>
+                                <i class="fa-solid fa-dollar-sign" style="color: #C2B280;"></i>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
 
-@endforeach
+            @endforeach
 
             @endif
 
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
+                crossorigin="anonymous"></script>
+            </b ody>
 
-
-
-
-
-
-        <div class="background-icons">
-            <img src="{{asset('web/imgs/Shape 3 copy 2.svg')}}" alt="" class=" shape shape1">
-            <img src="{{asset('web/imgs/2-layers (1).svg')}}"alt="" class="shape shape2">
-            <img src="{{asset('web/imgs/Shape 2 copy 2.svg')}}" alt="" class="shape shape4">
-            <img src="{{asset('web/imgs/2-layers.svg')}}" alt="" class="shape shape5">
-            <img src="{{asset('web/imgs/3-layers.svg')}}"
-            alt="" class="shape shape6">
-            <img src="{{asset('web/imgs/Shape 3 copy 2.svg')}}" alt="" class=" shape shape7">
-                 <img src="   {{asset('web/imgs/1-layers.svg')}}" alt="" class=" shape shape8">
-            <img src="{{asset('web/imgs/Shape_1_copy.svg')}}" alt="" class=" shape shape10">
-            <img src="{{asset('web/imgs/Shape_1_copy.svg')}}" alt="" class=" shape shape11">
-            {{-- <img src="{{asset('web/imgs/2-layers (1).svg')}}" alt="" class="shape shape12"> --}}
-            <img src="{{asset('web/imgs/2-layers (1).svg')}}" alt="" class="shape shape13">
-            <img src=" {{asset('web/imgs/4-layers.svg')}}" alt="" class="shape shape14">
-            <img src="{{asset('web/imgs/Shape 4 copy 2.svg')}}" alt="" class="shape shape15">
-            <img src="{{asset('web/imgs/5-layers.svg')}}" alt="" class="shape shape16">
-            <img src="{{asset('web/imgs/3-layers.svg')}}" alt="" class="shape shape17">
-            <img src="{{asset('web/imgs/Shape_2_copy_2.svg')}}" alt="" class="shape shape18">
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-            crossorigin="anonymous"></script>
-    </body>
 
 </html>
--- resources/views/front-end/pageAR.blade.php
+++ resources/views/front-end/pageAR.blade.php
@@ -8,28 +8,37 @@
         <title>Document</title>
         <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{asset('web/css/style.css')}}" >
+        <link rel="stylesheet" href="{{asset('web/css/style.css')}}">
+        <style>
+        body {
+            background-image: url("{{url('/')}}/web/imgs/31-layers.png");
+            background-size: 115%;
+            background-repeat: repeat-y;
+            background-position-x: -30px;
+
+        }
+
+        @media (min-width: 576px) {
+            body {
+                background-size: 110%;
+            }
+        }
+        </style>
     </head>
+
     <body>
         <div class="title">
             <div class="container">
-                <div class="col-12">
-                    @php
-                      $url = url()->full();
-                      $pos = strpos($url, app()->getLocale());
-                    @endphp
-                    <a href="{{ substr_replace($url,"en",$pos,2) }}" class="btn btn-info"> English</a>
-                    <a href="{{ substr_replace($url,"ar",$pos,2) }}" class="btn btn-primary"> Arabic</a>
-                    </div>
                 <div class="logo text-center w-50">
-                    <img src="{{asset('web/imgs/logo.png')}}" alt="" class="w-100">
+                    <img src="{{asset('web/imgs/logo.png')}}" alt="">
                 </div>
             </div>
         </div>
-        @foreach ($x as $index=>$category)
+
         <div class="menu-list ">
+            @foreach ($x as $index=>$category)
             <div class="menu-item">
-                <div class="container">
+                <div class="container menu-item-wrapper">
                     <a href="{{url('catageries/show')}}/{{$category->id}}">
                         <div class="row gx-1">
                             <div class="col-9">
@@ -38,8 +47,10 @@
                                 </div>
                             </div>
                             <div class="col-3">
-                                <img src="{{ url('public/Image/'.$category->image) }}"
-                                style="height: 200px; width: 150px;">
+                                <div class="item-img-wrapper">
+                                    <img src="{{ url('public/Image/'.$category->image) }}" class="item-img">
+                                </div>
+
                             </div>
                         </div>
                     </a>
@@ -47,32 +58,13 @@
             </div>
 
 
+            @endforeach
+        </div>
 
-        </div>
-        @endforeach
-        <div class="background-icons">
-            <img src="{{asset('web/imgs/Shape 3 copy 2.svg')}}" alt="" class=" shape shape1">
-            <img src="{{asset('web/imgs/2-layers (1).svg')}}" alt="" class="shape shape2">
-            <img src="{{asset('web/imgs/Shape 1 copy 3.svg')}}" alt="" class=" shape shape3">
-            <img src="{{asset('web/imgs/Shape 2 copy 2.svg')}}" alt="" class="shape shape4">
-            <img src="{{asset('web/imgs/2-layers.svg')}}" alt="" class="shape shape5">
-            <img src="{{asset('web/imgs/3-layers.svg')}}" alt="" class="shape shape6">
-            <img src="{{asset('web/imgs/Shape 3 copy 2.svg')}}" alt="" class=" shape shape7">
-            <img src="{{asset('web/imgs/1-layers.svg')}}" alt="" class=" shape shape8">
-            <img src="{{asset('web/imgs/Shape_1_copy.svg')}}" alt="" class=" shape shape10">
-            <img src="{{asset('web/imgs/Shape_1_copy.svg')}}" alt="" class=" shape shape11">
-            <img src="{{asset('web/imgs/2-layers (1).svg')}}" alt="" class="shape shape12">
-            <img src="{{asset('web/imgs/2-layers (1).svg')}}" alt="" class="shape shape13">
-            <img src="{{asset('web//4-layers.svg')}}" alt="" class="shape shape14">
-            <img src="{{asset('web/imgs/Shape 4 copy 2.svg')}}" alt="" class="shape shape15">
-            <img src="{{asset('web/imgs/5-layers.svg')}}" alt="" class="shape shape16">
-            <img src="{{asset('web/imgs/3-layers.svg')}}" alt="" class="shape shape17">
-            <img src="{{asset('web/imgs/Shape_2_copy_2.svg')}}" alt="" class="shape shape18">
-        </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
-            crossorigin="anonymous"></script>
+            integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+        </script>
     </body>
 
 </html>
